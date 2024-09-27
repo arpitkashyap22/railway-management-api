1dfba8bc123e6cd93c6eb3892dd5f0bc1b6ad06f
arpitkashyap22 <arpitkashyap2000@gmail.com>
2024-09-28 01:38:58 +0530

    fixed bearer split

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,9 +1,16 @@
 import jwt from 'jsonwebtoken';
 
 export const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) return res.status(403).json({ error: 'No token provided' });
+    const authHeader = req.headers['authorization'];
 
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(403).json({ error: 'No token provided or incorrect format' });
+    }
+  
+    // Extract token by removing 'Bearer ' from the header
+    const token = authHeader.split(' ')[1];
+    
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) return res.status(403).json({ error: 'Invalid token' });
     req.user = decoded;
